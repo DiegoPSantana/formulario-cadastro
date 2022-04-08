2075ea92da8030761f42df9724773d9f434180f5
DiegoPSantana <diego.fourstage@gmail.com>
2022-04-08 16:41:35 -0300

    Fechando a Sessão ao fechar o navegador

--- sistema.php
+++ sistema.php
@@ -1,12 +1,38 @@
+<?php
+session_start();
+print_r($_SESSION);
+
+// Se estiver logado na minha sessão o email e senha tal
+// iremos matar a sessão para questão de segurança
+// assim enviando usuário para refazer o login
+
+if ((!isset($_SESSION['emial']) == true) and (!isset($_SESSION['senha']) == true)) {
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    header('Location: tela-de-login.php');
+
+    // Caso caia nessa função ele matará a sessão e retornar a pagina de login
+}
+
+// Agora se você estiver logado ele vai continuar logado
+// Para que sessão seja fechada você tem que fechar o navagador
+// que cairá na função acima, onde vai para a pagina de login
+$logado = $_SESSION['email'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
 </head>
+
 <body>
     <h1>Parabéns você acessou o sistema com sucesso !</h1>
 </body>
+
 </html>
--- testLogin.php
+++ testLogin.php
@@ -1,40 +1,46 @@
 <?php
 
- //print_r($_REQUEST);  // Para visualizar no navegador os dados digitados
+session_start();
 
-    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
-    {
-            //Acessa
-            include_once('config.php');
-            $email = $_POST['email'];
-            $senha = $_POST['senha'];
+//print_r($_REQUEST);  // Para visualizar no navegador os dados digitados
 
-               // Para visualizar se os dados estão chegando 
-            // print_r('Email: ' . $email);
-            // print_r('<br>');
-            // print_r('Senha: ' . $senha);
+if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+   //Acessa
+   include_once('config.php');
+   $email = $_POST['email'];
+   $senha = $_POST['senha'];
 
-            // Verificar se os parametros existem no banco de dados
-            $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
+   // Para visualizar se os dados estão chegando 
+   // print_r('Email: ' . $email);
+   // print_r('<br>');
+   // print_r('Senha: ' . $senha);
 
-            $result = $conexao->query($sql);
-            
-            // print_r($sql);
-            // print_r('<br>');
-            // print_r($result);
+   // Verificar se os parametros existem no banco de dados
+   $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
 
-            if(mysqli_num_rows($result) < 1){
-               //print_r('Não existe');
-               header('Location: tela-de-login.php');
-            }
-            else{
-               //print_r('Existe');
+   $result = $conexao->query($sql);
 
-               header('Location: sistema.php');
-            }
-    }
-    else
-    {
-        // Acessa
-        header('Location: tela-de-login.php');
-    }
+   // print_r($sql);
+   // print_r('<br>');
+   // print_r($result);
+
+   if (mysqli_num_rows($result) < 1) {
+      //print_r('Não existe');
+      header('Location: tela-de-login.php');
+
+      // Aqui ele vai matar a sessão
+      unset($_SESSION['email']);
+      unset($_SESSION['senha']);
+
+   } else {
+      //print_r('Existe');
+      header('Location: sistema.php');
+      
+      // Aqui ele vai abrir a sessão
+      $_SESSION['email'] = $email;
+      $_SESSION['senha'] = $senha;
+   }
+} else {
+   // Acessa
+   header('Location: tela-de-login.php');
+}
